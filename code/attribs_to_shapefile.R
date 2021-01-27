efd781a38c14b095c0f4084fa7d2f826b1a884cc
Jens Stevens <jtstevens@usgs.gov>
2021-01-27 15:40:27 -0500

    Initial attribution of T30,31 (R11)

--- code/attribs_to_shapefile.R
+++ code/attribs_to_shapefile.R
@@ -5,22 +5,21 @@ library(tidyverse)
 library(readxl)
 
 #Read spatial data
-lines <- read_sf("./GIS/section lines/T29R11_lines.shp")
-#lines_sf <- st_cast(lines)
+id <- "T31R11" #set the township you want to work on
+lines <- read_sf(paste0("./GIS/section lines/",id,"_lines.shp"))
 plot(lines$geometry)
 
 #Read survey data
-d_1940 <- read_xlsx("./data/T29R11_data.xlsx", sheet = "1940Survey")
+d_1940 <- read_xlsx(paste0("./data/",id,"_data.xlsx"), sheet = "1940Survey")
 d_simple_1940 <- d_1940[d_1940$Survey_type=="transect_summary",c("Segment_id","Sage")]
 names(d_simple_1940)[2] = "Sage_1940"
-d_1880 <- read_xlsx("./data/T29R11_data.xlsx", sheet = "1880Survey")
+d_1880 <- read_xlsx(paste0("./data/",id,"_data.xlsx"), sheet = "1880Survey")
 d_simple_1880 <- d_1880[d_1880$Survey_type=="transect_summary",c("Segment_id","Sage")]
 names(d_simple_1880)[2] = "Sage_1880"
 d_simple <- merge.data.frame(d_simple_1940,d_simple_1880, all = TRUE)
-#d_simple <- cbind(d_simple_1940,d_simple_1880)
 
-#tmp <- left_join(lines,d[d$Survey_type=="transect_summary",c("Segment_id","Sage")])
 attribs <- left_join(lines,as.data.frame(d_simple))
-plot(attribs["Sage"])
+plot(attribs["Sage_1940"])
+plot(attribs["Sage_1880"])
 
-write_sf(attribs,"./GIS/section lines/T29R11_lines_attrib.shp")
+write_sf(attribs,paste0("./GIS/section lines/",id,"_lines_attrib.shp"))
