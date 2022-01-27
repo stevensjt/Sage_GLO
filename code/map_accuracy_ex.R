9872308385e4ab75fef3f8a55d3285fd03480abc
kf1459 <karafox31@gmail.com>
2022-01-27 09:11:07 -0700

    Update for R script

--- code/map_accuracy_ex.R
+++ code/map_accuracy_ex.R
@@ -17,7 +17,20 @@ accuracy(observed[sample(1:length(observed))], observed)
 
 
 #2 Two classes (contingency table)
-accuracy(cbind(c(15,11), c(2,123))) #this is the easiest data format to provide - cbind(c(number true positive, number false positive), (number false negative, number false positive))
+accuracy(cbind(c(15,11), c(2,123))) #this is the easiest data format to provide - cbind(c(number true positive, number false positive), (number false negative, number true negative))
+
+#landfire1
+accuracy(cbind(c(61,68), c(5,48)))
+#landfire2
+accuracy(cbind(c(61,68), c(7,46)))
+#RCMAP1
+accuracy(cbind(c(95,34), c(13,40)))
+#RCMAP2
+accuracy(cbind(c(75,54), c(7,46)))
+#RCMAP3
+accuracy(cbind(c(84,45), c(11,42)))
+#RCMAP4
+accuracy(cbind(c(93,36), c(13,40)))
 
 #3 Multiple classes
 accuracy(iris[sample(1:150),]$Species, iris$Species)
