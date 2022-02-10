d96969484b248f8529179980f0a65909cc6059e9
kf1459 <karafox31@gmail.com>
2022-02-10 14:52:51 -0700

    updating excel and code

--- code/attribs_to_shapefile_v2.R
+++ code/attribs_to_shapefile_v2.R
@@ -1,6 +1,7 @@
 ##Combine the sage data from datasheets into the section lines shapefiles
 ##Jens Stevens, stevensjt@gmail.com, 1/26/21
 #eqm and kf edits to add 2004 data 10/19/21
+#kmf edits to reflect 2019 data 02/10/22
 
 library(sf)
 library(tidyverse)
@@ -22,12 +23,12 @@ d_1940 <- read_xlsx(paste0("./data/",id,"_data.xlsx"), sheet = "1940Survey") %>%
   filter(Survey_type == "transect_summary") %>%
   select(Segment_id,Sage) %>%
   rename(Sage_1940 = Sage)
-d_2004 <- read_xlsx(paste0("./data/",id,"_data.xlsx"), sheet = "2004Survey") %>%
+d_2019 <- read_xlsx(paste0("./data/",id,"_data.xlsx"), sheet = "2019Survey") %>%
   filter(Survey_type == "transect_summary") %>%
   select(Segment_id,Sage) %>%
-  rename(Sage_2004 = Sage)
+  rename(Sage_2019 = Sage)
 d_simple <- full_join(d_1940,d_1880)
-d_simple<-full_join(d_simple, d_2004) #add 2004 data
+d_simple<-full_join(d_simple, d_2019) #add 2019 data
 
 #don't think this is needed - just re-creating d_simple
 #d_simple_1940 <- d_1940[d_1940$Survey_type=="transect_summary",c("Segment_id","Sage")]
@@ -40,7 +41,7 @@ d_simple<-full_join(d_simple, d_2004) #add 2004 data
 attribs <- left_join(lines,as.data.frame(d_simple))
 plot(attribs["Sage_1880"])
 plot(attribs["Sage_1940"])
-plot(attribs["Sage_2004"])
+plot(attribs["Sage_2019"])
 
 write_sf(attribs,paste0("./GIS/section lines/",id,"_lines_attrib.shp"))
 
@@ -58,17 +59,17 @@ for(id in ids){
     filter(Survey_type == "transect_summary") %>%
     select(Segment_id,Sage) %>%
     rename(Sage_1940 = Sage)
-  d_2004 <- read_xlsx(paste0("./data/",id,"_data.xlsx"), sheet = "2004Survey") %>%
+  d_2019 <- read_xlsx(paste0("./data/",id,"_data.xlsx"), sheet = "2019Survey") %>%
     filter(Survey_type == "transect_summary") %>%
     select(Segment_id,Sage) %>%
-    rename(Sage_2004 = Sage)
+    rename(Sage_2019 = Sage)
 
     lines <- read_sf(paste0("./GIS/section lines/",id,"_lines.shp")) %>%
     select(lndkey,sectn,Segment_id,geometry)
   
   
   l[[id]] <- full_join(d_1880,d_1940)#join 1940 & 1880
-  l[[id]] <- full_join(l[[id]],d_2004)#add 2004
+  l[[id]] <- full_join(l[[id]],d_2019)#add 2019
 
     if(id == ids[1]){
     d_full <- l[[id]]
@@ -83,19 +84,19 @@ for(id in ids){
 attribs <- left_join(lines_full,d_full)
 plot(attribs["Sage_1880"])
 plot(attribs["Sage_1940"])
-plot(attribs["Sage_2004"])
+plot(attribs["Sage_2019"])
 
 #adding new "simple" columns for all years, simplifying the different sage observations to "present" and absent 
 attribs$Sage_1880_simple <- ifelse(attribs$Sage_1880%in%c("present","understory","dense"),
                                    "present",attribs$Sage_1880)
 attribs$Sage_1940_simple <- ifelse(attribs$Sage_1940%in%c("present","understory","dense"),
                                    "present",attribs$Sage_1940)
-attribs$Sage_2004_simple <- ifelse(attribs$Sage_1940%in%c("present","understory","dense"),
-                                   "present",attribs$Sage_2004)
+attribs$Sage_2019_simple <- ifelse(attribs$Sage_1940%in%c("present","understory","dense"),
+                                   "present",attribs$Sage_2019)
 
 plot(attribs["Sage_1880_simple"])
 plot(attribs["Sage_1940_simple"])
-plot(attribs["Sage_2004_simple"])
+plot(attribs["Sage_2019_simple"])
 
 #write the final kml & shp
 write_sf(attribs,paste0("./GIS/section lines/all_lines_attrib_v4.kml"))
@@ -104,7 +105,7 @@ write_sf(attribs,paste0("./GIS/section lines/all_lines_attrib_v4.shp"))
 #############draft maps of 3 yrs
 
 #make draft maps and save
-for(i in c(1880,1940,2004)){
+for(i in c(1880,1940,2019)){
 png(paste("./figures/",i,"_map.png", sep=""), width=800, height = 800)
 plot(attribs[paste("Sage_",i,"_simple",sep="")], lwd = 2)
 dev.off()
